3261d4e54902d0ac56cade79b4ca5c7ae5c57abe
jassperlawrence <54319817+jassperlawrence@users.noreply.github.com>
2020-02-26 00:44:06 +0800

    Update DSTuto1.java

--- DSTuto1.java
+++ DSTuto1.java
@@ -17,6 +17,8 @@ public class DSTuto1 {
     public static void main(String[] args) {
         Tarikh a = new Tarikh(05,11,2016);
         System.out.println(a.toString());
+        Sentence b= new Sentence ("How are you?");
+        System.out.println(b.toString()+"\nThe length of sentence : "+b.length()+"\nThe number of vowels : "+b.vowels()+"\nThe number of words : "+b.words());
     }
     
 }
