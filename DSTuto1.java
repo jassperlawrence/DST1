d57d6d765351952cef585d7a11cdc3c6516cc38d
jassperlawrence <54319817+jassperlawrence@users.noreply.github.com>
2020-02-26 21:21:35 +0800

    Update DSTuto1.java

--- DSTuto1.java
+++ DSTuto1.java
@@ -15,10 +15,24 @@ public class DSTuto1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //No.1
         Tarikh a = new Tarikh(05,11,2016);
         System.out.println(a.toString());
+        //No.2
         Sentence b= new Sentence ("How are you?");
         System.out.println(b.toString()+"\nThe length of sentence : "+b.length()+"\nThe number of vowels : "+b.vowels()+"\nThe number of words : "+b.words());
+        //No.3 T1
+        DPlan a = new DPlan (5,50,"5mbps Subscription and 50GB");
+        Mplan b = new Mplan(5,50,"5mbps Subscription and 50GB");
+        
+        System.out.println(a.display());
+        System.out.println(a.plan());
+        System.out.println(b.plan());
+        if(a.cost()<=b.cost()){
+            System.out.println("DPlan is cheaper compared to MPlan");
+        }
+        else
+           System.out.println("MPlan is cheaper compared to DPlan"); 
     }
     
 }
